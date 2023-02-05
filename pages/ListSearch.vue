9844ba36162eac4b47a6386e183e270072278578
藍允文 <nicklan@oetn.cfv>
2023-02-05 21:45:24 +0800

    autocomplete without tool

--- pages/ListSearch.vue
+++ pages/ListSearch.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
 div
   h1 {{ title }}
-  el-row
-    el-autocomplete(v-model.trim="inputNo" :fetch-suggestions="queryNo" placeholder="查詢編號" clearable class="search-input") 
-    el-button(@click="search" type="primary" class="large") 查詢
+  .search-group
+    //- --- with tool ---
+    //- el-autocomplete(v-model.trim="inputNo" :fetch-suggestions="queryNo" placeholder="查詢編號" clearable class="search-input")
+    //- el-button(@click="search" type="primary" class="large") 查詢
+    el-input(v-model.trim="inputNo" placeholder="查詢編號" clearable)
+      el-button(slot="append" icon="el-icon-search" @click="search")
+    ul.list(v-if="showList")
+      li(v-for="item in queryData" :key="item.value" @click="setInput(item.value)") {{ item.value }} 
 </template>
 
 <script>
@@ -15,21 +20,44 @@ export default {
     return {
       title: '查詢表單',
       inputNo: '',
+      showList: false,
+      queryData: [],
     }
   },
   computed: {
     ...mapState('data', ['tableData']),
   },
+  watch: {
+    inputNo() {
+      if (this.inputNo === '') this.showList = false
+      else {
+        this.showList = true
+        this.queryNo()
+      }
+    },
+  },
 
   methods: {
     ...mapActions('data', ['setDetailData']),
     ...mapActions('status', ['setAddFlag', 'setReadonly']),
-    queryNo(queryString, cb) {
-      const queryData = this.tableData
-      const results = queryString
-        ? queryData.filter((e) => e.value.includes(queryString))
-        : queryData
-      cb(results)
+    // with tool
+    // queryNo(queryString, cb) {
+    //   const queryData = this.tableData
+    //   const results = queryString
+    //     ? queryData.filter((e) => e.value.includes(queryString))
+    //     : queryData
+    //   cb(results)
+    // },
+    queryNo() {
+      const queryLength = 10
+      const resultData = this.tableData.filter((e) =>
+        e.value.includes(this.inputNo)
+      )
+      this.queryData = resultData.slice(0, queryLength)
+    },
+    async setInput(value) {
+      await (this.inputNo = value)
+      await (this.showList = false)
     },
     search() {
       if (this.inputNo === '') {
